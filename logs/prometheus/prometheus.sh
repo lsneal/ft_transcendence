e440fe50362c6825c429b6af7937ff5ac976b93b
Sloth <you@example.com>
2024-01-20 13:31:22 +0100

    try metrics db

--- logs/prometheus/prometheus.sh
+++ logs/prometheus/prometheus.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-node_exporter &
+if [ ! -f prometheus/prometheus ] 
+then 
+	wget https://github.com/prometheus/prometheus/releases/download/v2.49.0-rc.2/prometheus-2.49.0-rc.2.linux-amd64.tar.gz 
+	tar xvf prometheus-2.49.0-rc.2.linux-amd64.tar.gz
+	rm prometheus-2.49.0-rc.2.linux-amd64.tar.gz
+fi
 
-exec prometheus-2.49.0-rc.2.linux-amd64/prometheus --config.file=/etc/prometheus/prometheus.yml
+exec  prometheus-2.49.0-rc.2.linux-amd64/prometheus --config.file=/etc/prometheus/prometheus.yml
