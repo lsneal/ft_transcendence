c58c1b85db6d52b75a2f5921f60d7f02e86a4208
tt <tt@example.com>
2024-01-29 05:20:24 -0500

    GAME

--- site/app/site/app/consumers.py
+++ site/app/site/app/consumers.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time 
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
@@ -15,24 +16,47 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
-        print("ok", file=sys.stderr)
+        
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
+        moov = text_data_json['moov']
+                           
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type':'chat_message',
-                'message':message
+                'type':'game_moov',
+                'moov':moov
             }
         )
-    
-    def chat_message(self, event):
-        message = event['message']
+    i = 0
+    def game_moov(self, event):
+        moov = event['moov']
 
         self.send(text_data=json.dumps({
-            'type':'chat',
-            'message':message
+            'type':'game',
+            'moov':moov
         }))
+        if i < 100:
+            print("sleep: ", moov,file=sys.stderr)
+            time.sleep(1)
+
+#    def receive(self, text_data):
+#        text_data_json = json.loads(text_data)
+#        message = text_data_json['message']
+#
+#        async_to_sync(self.channel_layer.group_send)(
+#            self.room_group_name,
+#            {
+#                'type':'chat_message',
+#                'message':message
+#            }
+#        )
+#    
+#    def chat_message(self, event):
+#        message = event['message']
+#
+#        self.send(text_data=json.dumps({
+#            'type':'chat',
+#            'message':message
+#        }))
