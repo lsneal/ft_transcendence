d6cd9e5fa38bdddf5810399a68cf573c44921649
Sloth <you@example.com>
2024-02-10 12:42:11 +0100

    kibana nginx

--- site/init.sql
+++ site/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
     user_first_name VARCHAR(20) NOT NULL,
     user_last_name VARCHAR(20) NOT NULL,
     user_birthdate DATE NOT NULL,
-    created_on TIMESTAMP NOT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     insertion_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -17,11 +17,10 @@ INSERT INTO users(
     user_email,
     user_first_name,
     user_last_name,
-    user_birthdate,
-    created_on
+    user_birthdate
 )
 VALUES 
-    ('cwenga','cwenga@carml.ai','carmel','wenga','1990-09-20',CURRENT_TIMESTAMP),
-    ('smenguope','smenguope@carml.ai','suzie','menguope','1992-11-13',CURRENT_TIMESTAMP),
-    ('cdiogni','cdiogni@carml.ai','christian','diogni','1992-10-13',CURRENT_TIMESTAMP);
+    ('cwenga','cwenga@carml.ai','carmel','wenga','1990-09-20'),
+    ('smenguope','smenguope@carml.ai','suzie','menguope','1992-11-13'),
+    ('cdiogni','cdiogni@carml.ai','christian','diogni','1992-10-13');
 
