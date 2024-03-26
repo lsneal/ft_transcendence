6e6d58b243b3b83a9ee6afe7f2d4e395753e19e2
lulaens <toto@toto.com>
2024-03-26 14:20:02 +0100

    O

--- users/users/script/users/views.py
+++ users/users/script/users/views.py
@@ -5,6 +5,7 @@ from rest_framework.exceptions import AuthenticationFailed
 import rest_framework_simplejwt, datetime, jwt
 import json
 
+from .settings import SECRET_KEY
 from django.conf import settings
 from django.contrib.auth import authenticate, get_user
 from django.middleware import csrf
@@ -18,6 +19,45 @@ from .utils import gen_otp_url, gen_key_user
 import pyotp
 from django.shortcuts import render
 import qrcode
+
+from functools import wraps
+from rest_framework.decorators import api_view
+
+#def jwt_authentication(view_func):
+#    @wraps(view_func)
+#    def wrapper(request, *args, **kwargs):
+#        token = request.COOKIES.get('access_token')
+#
+#        if not token:
+#            raise AuthenticationFailed('Unauthenticated!')
+#
+#        try:
+#            access_token_obj = AccessToken(token)
+#        except:
+#            try:
+#                token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+#                user = User.objects.get(id=token['user_id'])
+#                refresh_token = user.token_refresh
+#                refresh = RefreshToken(refresh_token)
+#                access_token_obj = refresh.access_token
+#
+#                response = view_func(request, *args, **kwargs)
+#
+#                response.set_cookie(
+#                    key=settings.SIMPLE_JWT['AUTH_COOKIE'],
+#                    value=str(access_token_obj),
+#                    expires=settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'],
+#                    secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+#                    httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+#                    samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+#                )
+#
+#                return response
+#            except Exception as e:
+#                print('Error:', e)
+#                raise AuthenticationFailed('Invalid Token!')
+#
+#    return wrapper
 
 def qr_code(request):
     def get(request):
@@ -42,7 +82,7 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    
+
 class Login42View(APIView):
     def get(self, request):    
         client = AuthorizationCodeClient(
@@ -93,10 +133,15 @@ class Login42View(APIView):
 class LoginView(APIView):
     def get(self, request):
         token = request.COOKIES.get('access_token')
-
+        
         access_token_obj = AccessToken(token)
+        
         user_id=access_token_obj['user_id']
         user=User.objects.get(id=user_id)
+    
+        email = request.data.get('email', None)
+
+        print(f"email  {email}")
 
         response = Response()
         if user.a2f is True:
@@ -129,14 +174,15 @@ class LoginView(APIView):
             httponly = settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
             samesite = settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
         )
-        response.set_cookie(
-            key = settings.SIMPLE_JWT['REFRESH_COOKIE'],
-            value = data["refresh"],
-            expires = settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'],
-            secure = settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
-            httponly = settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
-            samesite = settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+        user.token_refresh = response.set_cookie(
+                 key = settings.SIMPLE_JWT['REFRESH_COOKIE'],
+                 value = data["refresh"],
+                 expires = settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'],
+                 secure = settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+                 httponly = settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+                 samesite = settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
         )
+        user.save() 
         csrf.get_token(request)
         response.data = {"Success" : "Login successfully","data":data}
         return response
@@ -245,23 +291,27 @@ class LoginA2F(APIView):
         totp = pyotp.TOTP(user.totp_key)
 
         response = Response()
+
         if totp.now() == user_code:
             response.data = { 'message': 'success' }
         else:
             response.data = { 'message': 'failure' }
         return response
-    
-def getAccessToken(request):
+
+
+
+def getAccessToken(self, request):
     token = request.COOKIES.get('access_token')
     if not token:
         raise AuthenticationFailed('Unauthenticated!')
 
     response = Response()
-
     try:
         access_token_obj = AccessToken(token)
     except:
-        refresh_token = request.COOKIES.get('refresh_token')
+        token = jwt.decode(jwt=token, key=SECRET_KEY, algorithms=["HS256"], options={"verify_signature": False})
+        user = User.objects.get(id=token['user_id'])
+        refresh_token = user.token_refresh
         try:
             refresh = RefreshToken(refresh_token)
             access_token_obj = refresh.access_token
@@ -329,9 +379,9 @@ class UserView(APIView):
             }
             
         else:
-            response, access_token_obj = getAccessToken(request)
+            response, access_token_obj = getAccessToken(self, request)
 
-        
+
             user_id=access_token_obj['user_id']
             user=User.objects.get(id=user_id)
             serialiazer = UserSerializer(user)
