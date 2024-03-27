873243e1d2d102685ae181b0d1ccd9d638b7558d
agoichon <axelgoichon@gmail.com>
2024-03-27 14:29:32 +0100

    osef

--- nginx/site/js/fetchChange.js
+++ nginx/site/js/fetchChange.js
@@ -1,9 +1,13 @@
 async function EventChange () {
     const CurrentPseudo = document.querySelector('#ActuallyPseudo').value;
-    const Newpseudo = document.querySelector('#ChangePseudo').value;
     const  oldpassword  = document.querySelector('#OldPassword').value;
     const  password  = document.querySelector('#ChangePassword').value;
     const  confirmpassword  = document.querySelector('#ConfirmChangePassword').value;
+
+
+    let myModalEl = document.getElementById('modalProfile');
+    let modal = bootstrap.Modal.getInstance(myModalEl);
+
 
     if(oldpassword == password && oldpassword != ''){
       const errorMessage = 'No changes made';
@@ -19,19 +23,6 @@ async function EventChange () {
       errorElement.style.display = 'block';
       return;
     }
-    else if(Newpseudo != '' && oldpassword == ''){
-      const errorMessage = 'Please enter your actually password for change your pseudo';
-      const errorElement = document.getElementById('error-message-Login');
-      errorElement.innerText = errorMessage;
-      errorElement.style.display = 'block';
-      return;
-    }else if (CurrentPseudo === '' && Newpseudo != ''){
-      const errorMessage = 'Please enter your actually pseudo for change your pseudo';
-      const errorElement = document.getElementById('error-message-Login');
-      errorElement.innerText = errorMessage;
-      errorElement.style.display = 'block';
-      return;
-    }
 
       const userresponse = await fetch('/api/users/user/', {
           method: 'GET',
@@ -42,7 +33,7 @@ async function EventChange () {
 
       const userData = await userresponse.json();
       if (userData.data.pseudo != CurrentPseudo){
-        const errorMessage = 'Please enter your actually pseudo for change your pseudo';
+        const errorMessage = 'Please enter your pseudo for change your password';
         const errorElement = document.getElementById('error-message-Login');
         errorElement.innerText = errorMessage;
         errorElement.style.display = 'block';
