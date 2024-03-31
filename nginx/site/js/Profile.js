48c4b57812a8c4d0a07cbe3f08eb8a9aa34cdbae
Sloth <you@example.com>
2024-03-31 15:26:09 +0200

    modif socket and disable reload

--- nginx/site/js/Profile.js
+++ nginx/site/js/Profile.js
@@ -35,26 +35,50 @@ export default class extends AbstractView {
         if (isAuthenticated === false || window.location.href.indexOf('profile') < -1) {
             return; 
         }
-   
-        document.getElementById("TournamentButton").addEventListener('click', event => {
-            window.history.pushState(null, "Tournament", "/tournament/");
-            window.dispatchEvent(new Event('popstate'));
-        });
-        document.getElementById("OnlineGameButton").addEventListener('click', event => {
-            window.history.pushState(null, "Online", "/online/");
-            window.dispatchEvent(new Event('popstate'));
-        });
-        document.getElementById("LocalGameButton").addEventListener('click', event => {
-            window.history.pushState(null, "local", "/local/");
-            window.dispatchEvent(new Event('popstate'));
-        });
-        document.getElementById("ButtonChange").addEventListener('click', EventChange);
-        document.getElementById("flexSwitchTwoFA").addEventListener('click', EventGetQRCode);
+        
+        let tournamentButton = document.getElementById("TournamentButton");
+        let onlineButton = document.getElementById("OnlineGameButton");
+        let localButton = document.getElementById("LocalGameButton");
+        if (tournamentButton)
+        {
+            tournamentButton.addEventListener('click', event => {
+                window.history.pushState(null, "Tournament", "/tournament/");
+                window.dispatchEvent(new Event('popstate'));
+            });
+        }
+        if (onlineButton)
+        {
+            onlineButton.addEventListener('click', event => {
+                window.history.pushState(null, "Online", "/online/");
+                window.dispatchEvent(new Event('popstate'));
+            });
+        }
+        if (localButton)
+        {
+            localButton.addEventListener('click', event => {
+                window.history.pushState(null, "local", "/local/");
+                window.dispatchEvent(new Event('popstate'));
+            });
+        }
 
-        document.getElementById("Valid2FA").addEventListener('click', EventActiveTwoFA);
-        document.getElementById("btnScoreboard").addEventListener('click', getUserStats);
-        document.getElementById("confirmDisable2FA").addEventListener('click', EventDisableTwoFA);
-        document.getElementById("btnDropdown2").addEventListener('click', getHistoricOnline);
+        let buttonChange = document.getElementById("ButtonChange");
+        let flexSwitchTwoFA = document.getElementById("flexSwitchTwoFA");
+        let valid2FA = document.getElementById("Valid2FA");
+        let btnScoreboard = document.getElementById("btnScoreboard");
+        let confirmDisable2FA = document.getElementById("confirmDisable2FA");
+        let btnDropdown2 = document.getElementById("btnDropdown2");
+        if (buttonChange) {buttonChange.addEventListener('click', EventChange);}
+        if (flexSwitchTwoFA) {flexSwitchTwoFA.addEventListener('click', EventGetQRCode);}
+        if (valid2FA) {valid2FA.addEventListener('click', EventActiveTwoFA);}
+        if (btnScoreboard) {btnScoreboard.addEventListener('click', getUserStats);}
+        if (confirmDisable2FA) {confirmDisable2FA.addEventListener('click', EventDisableTwoFA);}
+        if (btnDropdown2) {btnDropdown2.addEventListener('click', getHistoricOnline);}
+        
+
+        
+        
+        
+        
         
     }
 
